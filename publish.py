45c5ec1b53ea78019fd3a277537d794deae32cbd
马式超 <shichao.ma@yiducloud.cn>
2018-09-06 16:12:07 +0800

    Delete: publish.py

--- publish.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-
-os.system("python setup.py sdist upload")
-os.system(f"git commit -am '{sys.argv[1]}' ")
-os.system("git push")
--- test/test_big_file_upload_and_download.py
+++ test/test_big_file_upload_and_download.py
@@ -44,7 +44,7 @@ class UploadController(Controller):
                             headers={"Content-Length": os.path.getsize(filename)})
 
 
-@pytest.fixture(scope="module", params=["a.zip", "test_in.py", "滋养细胞肿瘤.pdf"])
+@pytest.fixture(scope="module", params=["test_in.py", "滋养细胞肿瘤.pdf"])
 def filename(request):
     return request.param
 

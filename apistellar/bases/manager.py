89fad4f8e2969cd1e431348510feb310a1672de7
马式超 <shichao.ma@yiducloud.cn>
2019-01-21 13:04:52 +0800

    Feature: 优化console实现

--- apistellar/bases/manager.py
+++ apistellar/bases/manager.py
@@ -4,8 +4,9 @@ from apistar.server.asgi import ASGI_COMPONENTS
 from apistar.server.injector import ASyncInjector
 from apistar.server.validation import VALIDATION_COMPONENTS
 
-from .components import SettingsComponent, Component
-from ..helper import load_packages, find_children, STATE, INITIAL
+from apistellar.helper import load_packages, find_children, STATE, INITIAL
+from apistellar.bases.components import \
+    Component, SettingsComponent, ValidateRequestDataComponent
 
 
 class Manager(object):
@@ -23,6 +24,7 @@ class Manager(object):
         self.state = STATE
         self.state["app"] = self
         self.components = find_children(Component)
+        self.components.append(ValidateRequestDataComponent())
         self.injector = ASyncInjector(
             list(ASGI_COMPONENTS + VALIDATION_COMPONENTS) + self.components,
             INITIAL)
